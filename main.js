70e4e3d601786b76c0f553a5b6e8c03cd1d97fc4
osamaibhaiss <146546139+osamaibhaiss@users.noreply.github.com>
2024-02-24 07:04:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let x = document.querySelector("div");
+x.classList.add(".demo")
+let y = document.querySelector("div h1");
+y.classList.add(".vip");
+y.textContent="Hello"
+y.classList.remove(".vip");
